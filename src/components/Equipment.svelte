28fde348d2265810654fdc774542fc5c24760225
Chris Parent <chrisnparent@gmail.com>
2023-06-25 11:09:51 -0400

    genericize items and wounds

--- src/components/Equipment.svelte
+++ src/components/Equipment.svelte
@@ -1,37 +1,10 @@
 <script>
-    import Item from "./Item.svelte";
-    import ListItem from "./ListItem.svelte";
-    import listActions from "../lib/listActions.js";
+    import Collection from "./Collection.svelte";
 
     export let model;
 
-    function addEquipment() {
-        model.equipment.push({ name: '', size: 1 });
+    function update() {
         model.equipment = model.equipment;
-    }
-
-    function moveEquipment(n, item) {
-        listActions.move(model.equipment, n, item);
-        model.equipment = model.equipment;
-    }
-
-    function removeEquipment(item) {
-        listActions.remove(model.equipment, item);
-        model.equipment = model.equipment;
-    }
-
-    function addWound() {
-        model.wounds.push({ name: '', size: 1 });
-        model.wounds = model.wounds;
-    }
-
-    function moveWound(n, item) {
-        listActions.move(model.wounds, n, item);
-        model.wounds = model.wounds;
-    }
-
-    function removeWound(item) {
-        listActions.remove(model.wounds, item);
         model.wounds = model.wounds;
     }
 
@@ -49,24 +22,6 @@
         'btn-dark';
 
 </script>
-<div class="d-flex align-items-end m-1">
-    <span>Equipment</span>
-    <span title="capacity" class="ml-auto btn {itemBtnStyle}">{totalItemSize}/{maxItemSize}</span>
-    <button on:click={addEquipment} class="ml-1 btn btn-dark">Add</button>
-</div>
-{#each model.equipment as item}
-    <ListItem item={item} move={moveEquipment} remove={removeEquipment}>
-        <Item bind:item={item}></Item>
-    </ListItem>
-{/each}
-<hr />
-<div class="d-flex align-items-end m-1">
-    <span>Wounds</span>
-    <span title="capacity" class="ml-auto btn {woundBtnStyle}">{totalWoundSize}/{maxWoundSize}</span>
-    <button on:click={addWound} class="ml-1 btn btn-dark">Add</button>
-</div>
-{#each model.wounds as wound}
-    <ListItem item={wound} move={moveWound} remove={removeWound}>
-        <Item bind:item={wound} btnStyle="btn-danger"></Item>
-    </ListItem>
-{/each}
+<Collection model={model.equipment} capacity={maxItemSize} btnStyle={itemBtnStyle} {update} title="Equipment"/>
+<hr/>
+<Collection model={model.wounds} capacity={maxWoundSize} btnStyle={woundBtnStyle} {update} title="Wounds"/>
